ccd8866149577ed2824b51028a9aef7b9a076071
mksolutionsuniversal <291152090+mksolutionsuniversal@users.noreply.github.com>
2026-08-18 20:28:17 +0100

    feat: add confirmed AppleSMC write ioctl

--- src/BootCampPerformanceControl/FanControl/Smc/CrystalIdea/CrystalIdeaAppleSmcIoctl.cs
+++ src/BootCampPerformanceControl/FanControl/Smc/CrystalIdea/CrystalIdeaAppleSmcIoctl.cs
@@ -3,6 +3,7 @@ namespace BootCampPerformanceControl.FanControl.Smc.CrystalIdea;
 internal static class CrystalIdeaAppleSmcIoctl
 {
     internal const uint ReadKey = 0x220000;
+    internal const uint WriteKey = 0x220004;
     internal const uint GetKeyInfo = 0x22000C;
     internal const uint GetProtocol = 0x220020;
 }
