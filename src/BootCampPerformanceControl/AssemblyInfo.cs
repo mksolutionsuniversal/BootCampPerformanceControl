7fb1b60e40f328c680f13aa73b413ba9bfcd49d4
mksolutionsuniversal <291152090+mksolutionsuniversal@users.noreply.github.com>
2026-08-18 20:41:01 +0100

    test: expose internals to guarded SMC write round-trip tool

--- src/BootCampPerformanceControl/AssemblyInfo.cs
+++ src/BootCampPerformanceControl/AssemblyInfo.cs
@@ -3,6 +3,7 @@ using System.Windows;
 
 [assembly: InternalsVisibleTo("BootCampPerformanceControl.Tests")]
 [assembly: InternalsVisibleTo("BootCampPerformanceControl.SmcReadSmokeTest")]
+[assembly: InternalsVisibleTo("BootCampPerformanceControl.SmcWriteRoundTripTest")]
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None,
